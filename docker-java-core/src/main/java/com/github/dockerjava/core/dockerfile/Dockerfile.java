a6c4c504cbbf52d2b0fd0d0789e59dd521092afa
Andrian Sevastyanov <andrians@blackduck.com>
2025-05-30 11:10:40 -0600

    Copy symbolic links to directories as symlinks instead of cloning the directories

--- docker-java-core/src/main/java/com/github/dockerjava/core/dockerfile/Dockerfile.java
+++ docker-java-core/src/main/java/com/github/dockerjava/core/dockerfile/Dockerfile.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -203,7 +205,7 @@ public class Dockerfile {
 
             if (files.length != 0) {
                 for (File f : files) {
-                    if (f.isDirectory()) {
+                    if (f.isDirectory() && !Files.isSymbolicLink(Paths.get(f.getAbsolutePath()))) {
                         addFilesInDirectory(f);
                     } else if (effectiveMatchingIgnorePattern(f) == null) {
                         filesToAdd.add(f);
